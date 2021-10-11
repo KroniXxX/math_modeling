67897d64b41a404e584c6ebce1d6ca76bb5f47cd
KroniXxX <91481953+KroniXxX@users.noreply.github.com>
2021-10-11 15:46:13 +0000

    Мы что то умели и смогли написать код (нет)

--- p1_t3.py
+++ p1_t3.py
@@ -1,9 +1,9 @@
-'''
+
 from math import  sqrt
 x = 3 
 y = ((sqrt(x ** 3))/((x ** 3)+(3/x)))*((4 * x ** 7)-(x ** 5))+ 80 * sqrt((27 * x ** 4)+(12 * x ** 3)-(5 * x ** 2)+10)
 print (y)
-'''
+
 from math import sqrt
 x = 3
 b= (3%2)+((16.7 * 4.32)//1)
