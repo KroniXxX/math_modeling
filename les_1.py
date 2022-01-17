a640c975abc96817d8a1bd21244d161aea81e0de
KroniXxX <91481953+KroniXxX@users.noreply.github.com>
2022-01-17 14:53:54 +0000

    2123645

--- les_1.py
+++ les_1.py
@@ -1,3 +1,13 @@
-print()
-print('мы наверное молодцы')
-print()
+import numpy as np
+import matplotlib.pyplot as plt
+
+circle1 = plt.Circle((14 , 10), 1, color='y',fill=False)
+
+  
+
+fig, ax = plt.subplots() 
+ax.set_xlim((0, 20))
+ax.set_ylim((0, 20))
+ax.add_artist(circle1)
+
+fig.savefig('plotcircles12.png')
